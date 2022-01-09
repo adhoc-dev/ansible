b150ec64c6cfba6c46fffa8a108a652c17068b0f
dbollini <dib@adhoc.com.ar>
2022-01-09 15:06:32 -0300

    wrong copypaste

--- launch_project.sh
+++ launch_project.sh
@@ -27,8 +27,7 @@ echo '[Preparar notebook] Notebook lista!'
 
 # Deploy projecto Ansible, implementación
 echo "[Projecto Ansible] Clonar repositorio"
-git clone https://github.com/adhoc-dev/ansible.git
-cd ansible
+git clone https://github.com/adhoc-dev/ansible.git && cd ansible
 
 # Función para aplicar el rol de Ansible según quien use la notebook
 function Launch() {
@@ -41,16 +40,16 @@ read -e -p "Qué rol tendrá quien use este notebook? ('F'uncional, 'D'ev o 'S'y
 
 # Ejecutamos playbook / rol correspondiente
     if [[ "$USER_TYPE" == "F" ]]; then
-        $ ansible-playbook --tags "funcional" local.yml -K
+        ansible-playbook --tags "funcional" local.yml -K
     fi
 
 
     if [[ "$USER_TYPE" == "D" ]]; then
-        $ ansible-playbook --tags "devs" local.yml -K
+        ansible-playbook --tags "devs" local.yml -K
     fi
 
     if [[ "$USER_TYPE" == "S" ]]; then
-        $ ansible-playbook --tags "sysadmin" local.yml -K
+        ansible-playbook --tags "sysadmin" local.yml -K
     fi
 }
 
