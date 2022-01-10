2951f78b6c63479e7baa8c72c7b0f80a0223f54c
dbollini <dib@adhoc.com.ar>
2022-01-10 00:57:06 -0300

    add logs to script

--- launch_project.sh
+++ launch_project.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Para guardar logs
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
 
 # Script para preparar notebooks. Primero hace unas magias y luego aplica el rol de Ansible que corresponda
 
