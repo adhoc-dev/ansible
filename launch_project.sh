4a272040f723b4ee376d1f5d4a90c6f4d2d0ef8f
dbollini <dib@adhoc.com.ar>
2022-01-09 23:43:47 -0300

    launch with function because bash does not work

--- launch_project.sh
+++ launch_project.sh
@@ -29,5 +29,10 @@ echo "[Preparar notebook] Instalar colecciones de Ansible"
 ansible-galaxy collection install community.general
 
 # Para ejecutar el rol correspondiente, sin función ni validación (y bue)
+function launch() {
+
 read -e -p "Qué rol tendrá quien use este notebook? ('funcional', 'dev' o 'sysadmin' ): " USER_TYPE
 ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose
+}
+
+launch
