d8725bce67be5a38b90d82669b25bf718a451020
Diego Bollini <dib@adhoc.com.ar>
2022-05-30 15:20:32 -0300

    [IMP] clean code

--- launch_project.sh
+++ launch_project.sh
@@ -2,47 +2,50 @@
 # Script para preparar notebooks. Primero hace unas magias y luego aplica el rol de Ansible que corresponda
 
 # Actualizar sistema
-echo "[Preparar notebook] Actualizar ambiente de trabajo"
+echo "[PREPARAR NOTEBOOK] ACTUALIZAR AMBIENTE DE TRABAJO"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
 # Instalar requerimientos
-echo "[Preparar notebook] Instalar Git y Stow"
+echo "[PREPARAR NOTEBOOK] INSTALAR GIT Y STOW"
 sudo apt-get install -y git stow
 
 # Instalar dependencias de Ansible
-echo '[Preparar notebook] Instalar dependencias'
+echo '[PREPARAR NOTEBOOK] INSTALAR DEPENDENCIAS'
 sudo apt-get install -y python3-setuptools
 
 # Instalar Ansible
-echo "[Preparar notebook] Instalar Ansible con apt"
+echo "[PREPARAR NOTEBOOK] INSTALAR ANSIBLE"
 sudo apt-get install -y ansible
 
-echo '[Preparar notebook] Notebook lista!'
+echo '[PREPARAR NOTEBOOK] NOTEBOOK LISTA!'
 
 # Deploy projecto Ansible, implementación
-echo "[Projecto Ansible] Clonar repositorio"
+echo "[PROYECTO ANSIBLE] CLONAR REPOSITORIO"
 git clone https://github.com/adhoc-dev/ansible.git
 cd ansible
 
+[WARNING]: log file at /var/log/ansible.log is not writeable and we cannot create it, aborting
+
+
 # Para ejecutar el rol correspondiente, sin función ni validación (y bue)
 function launch {
-    read -e -p "Qué rol tendrá quien use este notebook? ('funcional', 'devs' o 'sysadmin' ): " USER_TYPE
+    read -e -p "PARA QUÉ ROL SE USARÁ ESTA NOTEBOOK? ('funcional', 'devs' o 'sysadmin' ): " USER_TYPE
 
     while [[ "$USER_TYPE" != "funcional" && "$USER_TYPE" != "devs" && "$USER_TYPE" != "sysadmin" ]]; do
         read -e -p "Ingresar el rol correctamente ('funcional', 'devs' o 'sysadmin'): " USER_TYPE
     done
 
     if [[ "$USER_TYPE" == "funcional" ]]; then
-        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose --user=$usuario
+        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose
     fi
 
     if [[ "$USER_TYPE" == "devs" ]]; then
-        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose --user=$usuario
+        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose
     fi
 
     if [[ "$USER_TYPE" == "sysadmin" ]]; then
-        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose --user=$usuario
+        ansible-playbook --tags "$USER_TYPE" local.yml -K --verbose
     fi
 }
 
